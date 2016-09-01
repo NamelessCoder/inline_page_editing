fcfd299fa539998bff38af1cbedf7a105cac399c
Claus Due <claus@namelesscoder.net>
2016-09-01 15:15:42 +0200

    [TASK] Add configuration for text, textpic

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,6 +1,8 @@
 <?php
 $GLOBALS['TCA']['tt_content']['types']['header']['inlineEditedFields'] = 'header';
 $GLOBALS['TCA']['tt_content']['types']['textmedia']['inlineEditedFields'] = 'header,bodytext';
+$GLOBALS['TCA']['tt_content']['types']['text']['inlineEditedFields'] = $GLOBALS['TCA']['tt_content']['types']['textmedia']['inlineEditedFields'];
+$GLOBALS['TCA']['tt_content']['types']['textpic']['inlineEditedFields'] = $GLOBALS['TCA']['tt_content']['types']['textmedia']['inlineEditedFields'];
 $GLOBALS['TCA']['tt_content']['types']['bullets']['inlineEditedFields'] = 'header,bullets_type,bodytext';
 $GLOBALS['TCA']['tt_content']['types']['table']['inlineEditedFields'] = 'header,table_delimiter,table_caption,bodytext,table_footer';
 $GLOBALS['TCA']['tt_content']['types']['uploads']['inlineEditedFields'] = 'header,media';
