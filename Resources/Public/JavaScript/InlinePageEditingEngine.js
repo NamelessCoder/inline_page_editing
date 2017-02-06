d6029109758b8b452c5a8e1217f3dc28fc5f9f2f
Claus Due <claus@namelesscoder.net>
2017-02-07 00:16:16 +0100

    [FEATURE] Show new content wizard as drop-down menu
    This feature adds a caret-triggered drop-down menu
    alongside all "new content" buttons. When clicked,
    instead of taking the user to the new content wizard,
    it loads the list of allowed content elements using XHR
    and presents the content types as a nice icon list with
    popover titles.

--- Resources/Public/JavaScript/InlinePageEditingEngine.js
+++ Resources/Public/JavaScript/InlinePageEditingEngine.js
@@ -38,7 +38,73 @@ define(['jquery'], function($) {
         }
     };
 
-    InlinePageEditing.clickAction = function() {
+    InlinePageEditing.convertToDropdown = function(element) {
+        element.wrapAll('<div class="btn-group new-content-trigger"></div>');
+        var dropdown = $(element).parent();
+        dropdown.append('<span class="btn btn-default btn-sm dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="sr-only">Toggle Dropdown</span><i class="fa fa-caret-down"></i> </span>');
+        dropdown.append('<div class="new-content-menu dropdown-menu"></div>');
+        dropdown.find('.dropdown-toggle').click(function() {
+            if (dropdown.find('.dropdown-menu').is(':visible')) {
+                return;
+            };
+            dropdown.find('.new-content-menu').html('<span class="t3js-icon icon icon-size-small icon-state-default icon-spinner-circle-light icon-spin" data-identifier="spinner-circle-light">' +
+                    '<span class="icon-markup">' +
+                    '<img src="/typo3/sysext/core/Resources/Public/Icons/T3Icons/spinner/spinner-circle-light.svg" width="16" height="16">' +
+                    '</span>' +
+                    '</span>'
+            );
+            var button = $(this);
+            var onclick = button.attr('onclick');
+            var url = element.attr('href');
+            $.ajax({
+                url: url,
+                complete: function(response) {
+                    var responseDocument = $("<div>" + response.responseText + "</div>");
+                    var form = responseDocument.find('#NewContentElementController');
+                    var gotoFunctionRegExp = new RegExp("function goToalt_doc\(\)[^\n]+\n([^\}]+)", "g");
+                    var gotoFunctionBody = gotoFunctionRegExp.exec(response.responseText)[2];
+                    eval('goToalt_doc = function() { ' + gotoFunctionBody.replace('return false;', '') + '; }');
+                    var icon;
+                    var link;
+                    $('.new-content-menu').html('');
+                    dropdown.find('.new-content-menu').append(form);
+                    form.hide();
+                    var tabTitles = form.find('.nav.nav-tabs.t3js-tabs a, .t3js-tabmenu-item a');
+                    var index = 0;
+                    var html = '<div class="row">';
+                    form.find('.tab-content div[role="tabpanel"]').each(function() {
+                        html += '<div class="col-xs-12 col-sm-6">';
+                        html += '<h4>' + $(tabTitles[index]).text() + '</h4>';
+                        var subIndex = 0;
+                        $(this).find('.media').each(function() {
+                            icon = $(this).find('.icon-markup img');
+                            link = $(this).find('a:first-child');
+                            link.attr('title', $(this).html());
+                            link.html('');
+                            link.css({whiteSpace: "nowrap"});
+                            link.prepend(icon).addClass('dropdown-item pull-left');
+                            subIndex++;
+                            html += link.wrap('<div>').parent().html();
+                            //$(html).prepend(link);
+
+
+                        });
+                        html += '</div>';
+                        if (index - 1 % 2 == 0 && index > 0) {
+                            html += '</div><div class="row">';
+                        }
+                        index++;
+                    });
+                    html += '</div>';
+                    dropdown.find('.new-content-menu').append(html);
+                    dropdown.find('a').tooltip({html: true});
+                    dropdown.find('.new-content-menu').css({width: dropdown.parent().parent().width()});
+                }
+            });
+        });
+    };
+
+    InlinePageEditing.clickEditAction = function() {
         var id = $(this).attr('data-element-id');
         var iframe = $('<iframe class="inline-editable" src="' + $(this).attr('data-edit-url') + '&element=' + id + '" id="' + id + '"></iframe>');
         if ($(this).attr('class').indexOf('open') >= 0) {
@@ -79,7 +145,7 @@ define(['jquery'], function($) {
     };
 
     InlinePageEditing.initializeEditEnableListeners = function() {
-        $(".inline-edit-click-enable").click(InlinePageEditing.clickAction);
+        $(".inline-edit-click-enable").click(InlinePageEditing.clickEditAction);
     };
 
     InlinePageEditing.initializeWindowEvents = function() {
@@ -89,7 +155,7 @@ define(['jquery'], function($) {
             } else if (e.data[0] == 'redirectTo') {
                 window.location.href = e.data[1];
             } else if (e.data[0] == 'closeFrame') {
-                $('#' + e.data[1]).parent().removeClass('open').html(e.data[2]).click(InlinePageEditing.clickAction);
+                $('#' + e.data[1]).parent().removeClass('open').html(e.data[2]).click(InlinePageEditing.clickEditAction);
             }
         }, false);
     };
@@ -113,6 +179,7 @@ define(['jquery'], function($) {
     } else {
         $(InlinePageEditing.initializeWindowEvents);
         $(InlinePageEditing.initializeEditEnableListeners);
+        $(".t3js-page-new-ce a:first-child").each(function() { InlinePageEditing.convertToDropdown($(this)); });
     };
 
     return InlinePageEditing;
